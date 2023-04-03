a4f0a731d03c8e91394e71fb2dfc4b5eb8c736be
Dicky Li <dickyli338@gmail.com>
2023-04-03 15:30:28 -0400

    add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './components/App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import { store } from './hooks/redux'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -14,6 +15,8 @@ root.render(
     </Provider>
   </React.StrictMode>
 )
+
+serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
