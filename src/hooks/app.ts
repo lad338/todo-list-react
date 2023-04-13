3031ee33fae01f3d6cd5e3bc19003e9c497705dd
Dicky Li <dickyli338@gmail.com>
2023-04-12 17:31:23 -0700

    update to show error message on adding duplicated task

--- src/hooks/app.ts
+++ src/hooks/app.ts
@@ -4,6 +4,7 @@ import { initTasks } from './redux'
 import { IDBService } from './idb'
 import { EnqueueSnackbar } from 'notistack'
 import { trimTask } from '../utils/taskTItle'
+import { DexieError } from 'dexie'
 
 export const addTaskAndRefresh = async (
   dispatch: any,
@@ -17,10 +18,23 @@ export const addTaskAndRefresh = async (
   const title = (formData.get('new-task-title') || '').toString().trim()
   if (title !== '') {
     const repo = isOnline ? ApiService : IDBService
-    await repo.addTask(title)
-    formRef.current?.reset()
-    dispatch(initTasks())
-    enqueueSnackbar(`Added Task: ${trimTask(title, 30)}`)
+    try {
+      await repo.addTask(title)
+      formRef.current?.reset()
+      dispatch(initTasks())
+      enqueueSnackbar(`Added Task: ${trimTask(title, 30)}`)
+    } catch (e: any) {
+      if (e.name === 'ConstraintError') {
+        enqueueSnackbar(`Cannot add duplicated task: ${trimTask(title, 30)}`, {
+          variant: 'warning',
+        })
+      } else {
+        console.log(e)
+        enqueueSnackbar(`Cannot add task, unknown error`, {
+          variant: 'error',
+        })
+      }
+    }
   }
 }
 
